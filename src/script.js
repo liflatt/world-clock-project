a941830d36207b9d24c169ec3d631fb2cc1ca615
liflatt <lflatt1221@gmail.com>
2024-12-26 15:59:13 -0600

    Added JS for select city dropdown

--- src/script.js
+++ src/script.js
@@ -49,5 +49,41 @@ function updateTime() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+
+  if (cityTimeZone === "current") {
+    const cityTimeZone = moment.tz.guess();
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `<div class="city">
+    <div>
+      <h2>Current Location</h2>
+      <div class="date">${cityTime.format("MMMM	D, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div></div>
+    <a href="/">Show Default Cities</a>`;
+  } else {
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `<div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	D, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+  </div>
+  <a href="/"> Show Default Cities </a>`;
+  }
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
